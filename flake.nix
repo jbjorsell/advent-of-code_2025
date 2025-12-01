bf94845ea000431d82c4bb99fb1e25be0f69607b
jbjorsell <jbjorsell@outlook.com>
2025-12-01 13:07:12 +0100

    create repo

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      crane,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        lib = pkgs.lib;
+        stdenv = pkgs.stdenv;
+
+        # Rust
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+      in
+      rec {
+
+        devShell =
+          with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+
+              # Nix
+              nix-prefetch-git
+              nixfmt-rfc-style
+
+              # Rust
+              rustToolchain
+
+            ];
+            # LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ];
+            # UV_PYTHON = "${pkgs.python314}/bin/python3";
+          };
+      }
+    );
+
+}
