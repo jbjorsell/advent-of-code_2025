6ab51c81673e8870fa3ab2ca1a92dce4f187bb7d
jbjorsell <jbjorsell@outlook.com>
2025-12-01 23:29:54 +0100

    refactor more

--- src/day1/dial.rs
+++ src/day1/dial.rs
@@ -12,6 +12,15 @@ enum Direction {
     Right,
 }
 
+impl Direction {
+    fn sign(&self) -> i32 {
+        match self {
+            Direction::Left => -1,
+            Direction::Right => 1,
+        }
+    }
+}
+
 impl FromStr for Direction {
     type Err = eyre::Error;
 
@@ -52,23 +61,14 @@ impl Dial {
     pub fn turn(&mut self, turn: DialTurn) -> DialTurnResult {
         let full_laps = turn.amount / 100;
         let value_mod = turn.amount % 100;
-        let mut zeroes_passed = full_laps;
 
-        let left_turn_passed_zero =
-            turn.direction == Direction::Left && self.value != 0 && value_mod >= self.value;
+        let left_turn_passed_zero = turn.direction == Direction::Left && value_mod >= self.value;
         let right_turn_passed_zero =
             turn.direction == Direction::Right && (self.value + value_mod) >= 100;
+        let passed_zero = self.value != 0 && (left_turn_passed_zero || right_turn_passed_zero);
+        let zeroes_passed = full_laps + passed_zero as u32;
 
-        if left_turn_passed_zero || right_turn_passed_zero {
-            zeroes_passed += 1;
-        }
-
-        let unbounded_value = if turn.direction == Direction::Left {
-            self.value as i32 - value_mod as i32
-        } else {
-            self.value as i32 + value_mod as i32
-        };
-
+        let unbounded_value = self.value as i32 + turn.direction.sign() * value_mod as i32;
         self.value = unbounded_value.rem_euclid(100) as u32;
         assert!(self.value < 100);
 
